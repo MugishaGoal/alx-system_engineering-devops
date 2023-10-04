f667dac9b316e600dab013bcb9a20c94ede68fce
Mugisha Goal <degaullemugisha@gmail.com>
2023-10-04 04:30:05 -0700

    A Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,3 @@
 #!/usr/bin/env ruby
 
-# Checks if an argument is provided
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit(1)
-end
-
-# Defines the regular expression pattern
-pattern = /School/
-
-# Extracts the argument
-text = ARGV[0]
-
-# Checks if the pattern matches the text
-if text.match?(pattern)
-  puts text
-else
-  puts ""
-end
+puts ARGV[0].scan(/School/).join
