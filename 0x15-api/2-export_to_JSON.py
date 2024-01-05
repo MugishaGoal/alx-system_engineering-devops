f51357d35043876cfb5d96beb81d9ab6ffca687a
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-05 06:16:44 -0800

    A Python script to export data in the JSON format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,71 +1,61 @@
 #!/usr/bin/python3
 '''A Python script to export data in the JSON format'''
 
+import json
 import re
 import requests
 import sys
-import json
 
-"""The API's URL"""
+"""The base URL of the JSONPlaceholder API"""
 API_URL = 'https://jsonplaceholder.typicode.com'
 
-def export_to_json(user_id, user_name, todos):
-    """
-    Export user's tasks to a JSON file.
-
-    Args:
-    - user_id (int): The ID of the user.
-    - user_name (str): The username of the user.
-    - todos (list): List of tasks associated with the user.
-
-    Returns:
-    - None
-    """
-    filename = f"{user_id}.json"
-
-    tasks_data = {str(user_id): []}
-
-    for todo in todos:
-        task_info = {
-            "task": todo.get('title'),
-            "completed": todo.get('completed'),
-            "username": user_name
-        }
-        tasks_data[str(user_id)].append(task_info)
-
-    with open(filename, 'w') as json_file:
-        json.dump(tasks_data, json_file, indent=4)
-
-    print(f"Data exported to {filename}")
-
 if __name__ == '__main__':
-    """Check if command line arguments were provided"""
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
-        sys.exit(1)
+    """
+    Check if command line arguments were provided and proceed accordingly.
+    """
+    if len(sys.argv) > 1:
+        """
+        Checks if the provided argument is a valid user ID (numerical).
+        """
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            """
+            Extracts the user ID and convert it to an integer.
+            """
+            user_id = int(sys.argv[1])
 
-    """Check if the argument is a valid user ID"""
-    if re.fullmatch(r'\d+', sys.argv[1]):
-        user_id = int(sys.argv[1])
+            """
+            Makes an API call to retrieve the user information based
+            on the provided user ID.
+            """
+            user_data = requests.get('{}/users/{}'.format(API_URL, user_id)).json()
 
-        """Get the user's information from the API"""
-        user_res = requests.get(f'{API_URL}/users/{user_id}')
+            """
+            Makes another API call to retrieve the todos (tasks)
+            associated with the user.
+            """
+            todos_data = requests.get('{}/todos'.format(API_URL)).json()
 
-        if user_res.status_code == 200:
-            user_data = user_res.json()
-            user_name = user_data.get('username')
+            """Filters the todos for the given user"""
+            todos = list(filter(lambda x: x.get('userId') == user_id, todos_data))
 
-            """Fetch user's TODO list"""
-            todos_res = requests.get(f'{API_URL}/todos?userId={user_id}')
+            """Writes the data to a JSON file"""
+            with open('{}.json'.format(user_id), 'w') as file:
+                """
+                Maps todos to the desired JSON format for better organization.
+                """
+                user_tasks = list(map(
+                    lambda x: {
+                        'task': x.get('title'),
+                        'completed': x.get('completed'),
+                        'username': user_data.get('username')
+                    },
+                    todos
+                ))
+                user_data_formatted = {
+                    '{}'.format(user_id): user_tasks
+                }
 
-            if todos_res.status_code == 200:
-                todos_data = todos_res.json()
-
-                """Write the data to a JSON file"""
-                export_to_json(user_id, user_name, todos_data)
-            else:
-                print(f"Failed to fetch TODO list. Status code: {todos_res.status_code}")
-        else:
-            print(f"Failed to fetch user information. Status code: {user_res.status_code}")
-    else:
-        print('Invalid user ID format.')
+                """
+                Writes the structured JSON data to the file.
+                """
+                json.dump(user_data_formatted, file, indent=4)
