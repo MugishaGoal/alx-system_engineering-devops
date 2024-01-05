fb20648793bad684b77c02d83b891718490ef0b2
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-05 05:18:44 -0800

    A Python script that returns information about the todo list of a given emplyoyee

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python3
 """Python script that returns information about a given employee ID"""
+import re
 import requests
 import sys
 
+"""The API's URL"""
+API_URL = 'https://jsonplaceholder.typicode.com'
 
-def get_todo_list_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
+"""Check if the script is being run directly"""
+if __name__ == '__main__':
+    """Checks if command line arguments were provided"""
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            """Gets the user's information from the API"""
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('name')
 
-    """Fetching user information"""
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
+            """Filter todos for the given user"""
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
 
-    """Fetching user's TODO list"""
-    todo_response = requests.get(f"{base_url}/todos?userId={employee_id}")
-    todo_data = todo_response.json()
-
-    """Extracting completed and total tasks"""
-    completed_tasks = [task for task in todo_data if task['completed']]
-    total_tasks = len(todo_data)
-    completed_count = len(completed_tasks)
-
-    """Displaying employee's TODO list progress"""
-    print(f"Employee {user_data['name']} is done with tasks"
-          f"({completed_count}/{total_tasks}):")
-
-    """Displaying completed tasks"""
-    for task in completed_tasks:
-        print(f"    {task['title']}")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_todo_list_progress(employee_id)
+            """Print the user's tasks"""
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
