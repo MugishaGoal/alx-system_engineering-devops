3f229df2c4075b86dca6f662aba6d45f7ed110c6
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-16 13:50:06 -0800

    A function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""A function that queries Reddit API and returns the number of subscribers
-"""
+'''A function that queries Reddit API and returns the number of subscribers'''
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """Reddit API endpoint for getting subreddit information"""
-    url = 'https://www.reddit.com'
-    """Reddit's base API URL"""
-
-    """Set a custom User-Agent to avoid Too Many Requests error"""
-    headers = {
-            'Accept': 'application/json '
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-            'AppleWebKit/537.36 (KHTML, like Gecko) '
-            'Chrome/58.0.3029.110 Safari/537.3'
+    '''
+    A function that queries Reddit API and returns the number of subscribers
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
     }
 
-    """Make the GET request"""
-    response = requests.get(url, headers=headers)
-    allow_redirects=False
+    '''Make the API request'''
+    url = f'https://www.reddit.com/r/{subreddit}/about/.json'
+    response = requests.get(url, headers=api_headers, allow_redirects=False)
 
-    """Check if the response is successful (status code 200)"""
+    '''Check if the request was successful (status code 200)'''
     if response.status_code == 200:
-        """Parse the JSON response"""
-        data = response.json()
-
-        """Extract the number of subscribers"""
-        subscribers = data['data']['subscribers']
+        '''Parse the JSON response and extract the number of subscribers'''
+        subscribers = response.json().get('data', {}).get('subscribers', 0)
         return subscribers
-    else:
-        return 0
+
+    '''If the subreddit is invalid or the request fails, return 0'''
+    return 0
