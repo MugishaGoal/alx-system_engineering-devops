4e2589f287ffc575b25025d735a5b5f71e19e46a
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-16 14:13:49 -0800

    A function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,17 +13,22 @@ def number_of_subscribers(subreddit):
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
             'AppleWebKit/537.36 (KHTML, like Gecko)',
             'Chrome/97.0.4692.71',
-            'Safari/537.36',
+            'Safari/537.36'
+        ])
     }
 
     '''Make the API request'''
-    url = f'https://www.reddit.com/r/{subreddit}/about/.json'
-    response = requests.get(url, headers=api_headers, allow_redirects=False)
+    REDDIT_BASE_URL = 'https://www.reddit.com'
+    response = requests.get(
+            '{}/r/{}/about/.json'.format(REDDIT_BASE_URL, subreddit),
+            headers=api_headers,
+            allow_redirects=False
+    )
 
     '''Check if the request was successful (status code 200)'''
     if response.status_code == 200:
         '''Parse the JSON response and extract the number of subscribers'''
-        subscribers = response.json().get('data', {}).get('subscribers', 0)
+        subscribers = response.json()['data']['subscribers']
         return subscribers
 
     '''If the subreddit is invalid or the request fails, return 0'''
