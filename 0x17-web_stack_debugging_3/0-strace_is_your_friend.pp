3a22560537958098f8247ba920ffef1c05ea595c
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-17 12:04:53 -0800

    An automated Puppet manifest to fix the 500 error of a wordpress site

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # An automated Puppet manifest to fix the 500 error of a wordpress site
 
-exec { 'fix-wordpress':
+exec { 'fixing-manifest':
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/  \
               /var/www/html/wp-settings.php; service apache2 restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
