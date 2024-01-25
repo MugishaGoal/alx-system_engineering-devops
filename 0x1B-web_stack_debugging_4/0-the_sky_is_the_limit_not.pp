183821d68e1f98039102a46a015c150abf6d7ef2
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-25 10:40:28 -0800

    Handles the issue of not handling multiple request at a time

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,26 +1,7 @@
-# The script fixes the issue of the server not handling multiple requests
+# Handles the servers issue of not handling multiple requests at a time
 
-# Install required packages
-package { 'nginx':
-  ensure => 'installed',
-}
-
-# Configure Nginx settings
-file { '/etc/nginx/nginx.conf':
-  ensure => file,
-  source => 'puppet:///path/to/nginx.conf',
-}
-
-# Adjust ULIMIT for Nginx
-exec { 'fix--for-nginx':
-  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' /etc/default/nginx; service nginx restart\"",
-  path    => '/usr/bin:/usr/sbin:/bin',
-  require => File['/etc/nginx/nginx.conf'],
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => [File['/etc/nginx/nginx.conf'], Exec['fix--for-nginx']],
+exec { 'fix-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
