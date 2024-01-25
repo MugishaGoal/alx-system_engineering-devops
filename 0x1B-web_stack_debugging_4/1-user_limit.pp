4321f9456d0ff5bd0a4067224d5041b39225bf0f
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-25 11:13:17 -0800

    Change the OS configuration so that it is possible to login with the holberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,8 +2,8 @@
 
 exec { 'Allows-holberton-user-to login':
   command => "sed -iE 's/^holberton hard nofile \
-.*/holberton hard nofile 88888/' /etc/security/limits.conf && \
-sed -iE 's/^holberton soft nofile .*/holberton soft nofile 88888/' \
+.*/holberton hard nofile 8192/' /etc/security/limits.conf && \
+sed -iE 's/^holberton soft nofile .*/holberton soft nofile 8192/' \
 /etc/security/limits.conf",
   path    => '/usr/bin:/usr/sbin:/bin',
 }
